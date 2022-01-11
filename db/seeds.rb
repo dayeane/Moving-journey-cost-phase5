8408e0151ecc7208bea36f4633fecb7361f9a83c
dayeane <dayeane85@gmail.com>
2022-01-11 16:08:01 -0500

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'date'
+puts "🌱 Seeding spices..."
+
+
+
+(0..5).each do |num|
+  puts "creating_trip #{num}"
+  trip1 = Trip.create(from: Faker::Address.city, to: Faker::Address.city, depart_date: Date.today, budget: 16)
+
+  (0..5).each do |num2|
+    puts "creating_stops #{num2}"
+    stop = trip1.stops.create(stop_reason: "Gas", note: Faker::Movies::StarWars.quote)
+    stop.create_cost(amount: Faker::Number.number(digits: 3), trip: trip1)
+  end
+
+  (0..5).each do |num3|
+    puts "creating_hotels #{num3}"
+    hotel = trip1.hotels.create(check_in: Date.today, check_out: Date.tomorrow, location: Faker::Address.city)
+    hotel.create_cost(amount: Faker::Number.number(digits: 3), trip: trip1)
+  end
+
+  (0..5).each do |num4|
+    puts "creating_provider #{num4}"
+    provider = trip1.providers.create(date: Date.tomorrow, note: Faker::Movies::StarWars.quote, name: Faker::Company.name)
+    provider.create_cost(amount: Faker::Number.number(digits: 3), trip: trip1)
+  end
+
+
+end
+
+puts "✅ Done seeding!"

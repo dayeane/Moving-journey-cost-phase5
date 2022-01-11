cf95970a81865e26e898cd78462bce60fc869ef2
dayeane <dayeane85@gmail.com>
2022-01-11 15:15:21 -0500

    create hotels

---
+++ db/migrate/20220111200735_create_hotel.rb
@@ -0,0 +1,12 @@
+class CreateHotel < ActiveRecord::Migration[6.1]
+  def change
+    create_table :hotels do |t|
+      t.references :trip
+      t.date :check_in
+      t.date :check_out
+      t.string :location
+
+      t.timestamps
+    end
+  end
+end

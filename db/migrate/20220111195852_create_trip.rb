3b33896c620f5db09d23d602f8e1f100ed22e9ba
dayeane <dayeane85@gmail.com>
2022-01-11 15:15:12 -0500

    create trips

---
+++ db/migrate/20220111195852_create_trip.rb
@@ -0,0 +1,12 @@
+class CreateTrip < ActiveRecord::Migration[6.1]
+  def change
+    create_table :trips do |t|
+      t.string :from
+      t.string :to
+      t.date :depart_date
+      t.integer :budget
+
+      t.timestamps
+    end
+  end
+end

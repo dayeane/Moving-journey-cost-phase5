2d94d1c086725dbf5e43e2854bcf1220a9dad2be
dayeane <dayeane85@gmail.com>
2022-01-13 10:04:03 -0500

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  
+
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
+  resources :trips, except: %i[new edit] do
+    resources :hotels, except: %i[new edit]
+    resources :providers, except: %i[new edit]
+    resources :stops, except: %i[new edit]
+  end
 end

66f934776609ef94a26bb9a88414c5a68e1e2ebc
dayeane <dayeane85@gmail.com>
2022-01-19 17:51:05 -0500

    create componet Map and move code

---
+++ client/src/Map.js
@@ -0,0 +1,49 @@
+import {useState} from 'react'
+import ReactMapGL, { Marker } from 'react-map-gl';
+
+import point_icon from './assets/point.png'
+import hotel_icon from './assets/hotel.png'
+
+export default function Map({trip}) {
+  const MAPBOX_TOKEN = 'pk.eyJ1IjoiYWZ1cm8iLCJhIjoiY2t5ZW4zOWFpMGRqczJxcWtheWNvZHQ2aiJ9.hn3zYWxYhJZv0YcZAVQcsA'
+  const [viewport, setViewport] = useState({
+    latitude: trip.from_latitude,
+    longitude: trip.from_longitude,
+    zoom: 11
+  });
+
+  return (
+    <ReactMapGL
+      {...viewport}
+      className='map'
+      width="100%"
+      height="400px"
+      mapboxApiAccessToken={MAPBOX_TOKEN}
+      onViewportChange={(viewport) => setViewport(viewport)}
+    >
+      <Marker key='11111' latitude={trip.from_latitude} longitude={trip.from_longitude}>
+        <button class='marker-button'>
+          <img className='marker-button' src={point_icon} alt='FROM'/>
+        </button>
+      </Marker>
+
+      <Marker key='2222' latitude={trip.to_latitude} longitude={trip.to_longitude}>
+        <button class='marker-button'>
+          <img className='marker-button' src={point_icon} alt='TO'/>
+        </button>
+      </Marker>
+
+      {trip.hotels.map((hotel) => {
+        if (hotel.latitude && hotel.longitude) {
+          return (
+            <Marker key={hotel.id} latitude={hotel.latitude} longitude={hotel.longitude}>
+            <button class='marker-button'>
+              <img className='marker-button' src={hotel_icon} alt='hotel'/>
+            </button>
+          </Marker>
+          )
+        } else { return null }
+      })}
+    </ReactMapGL>
+  )
+}
--- client/src/TripSummary.js
+++ client/src/TripSummary.js
@@ -1,59 +1,10 @@
 import {useState} from 'react'
 import TripEditForm from "./TripEditForm";
-import MapGL, { Source, Layer, Marker } from '@urbica/react-map-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
+import Map from './Map'
 
 function TripSummary({trip}) {
   const [currentTrip, setCurrentTrip] = useState(trip)
   const [editMode, setEditMode] = useState(false)
-  const [viewport, setViewport] = useState({
-    latitude: currentTrip.from_latitude,
-    longitude: currentTrip.from_longitude,
-    zoom: 11
-  });
-
-  const trip_route = {
-    type: "Feature",
-    properties: {},
-    geometry: {
-        type: "LineString",
-        coordinates: [
-            [currentTrip.from_latitude, currentTrip.from_longitude],
-            [currentTrip.to_latitude, currentTrip.to_longitude]
-        ]
-    }
-  };
-
-  const data = {
-    type: 'Feature',
-    geometry: {
-      type: 'LineString',
-      coordinates: [
-        [-122.48369693756104, 37.83381888486939],
-        [-122.48348236083984, 37.83317489144141],
-        [-122.48339653015138, 37.83270036637107],
-        [-122.48356819152832, 37.832056363179625],
-        [-122.48404026031496, 37.83114119107971],
-        [-122.48404026031496, 37.83049717427869],
-        [-122.48348236083984, 37.829920943955045],
-        [-122.48356819152832, 37.82954808664175],
-        [-122.48507022857666, 37.82944639795659],
-        [-122.48610019683838, 37.82880236636284],
-        [-122.48695850372314, 37.82931081282506],
-        [-122.48700141906738, 37.83080223556934],
-        [-122.48751640319824, 37.83168351665737],
-        [-122.48803138732912, 37.832158048267786],
-        [-122.48888969421387, 37.83297152392784],
-        [-122.48987674713133, 37.83263257682617],
-        [-122.49043464660643, 37.832937629287755],
-        [-122.49125003814696, 37.832429207817725],
-        [-122.49163627624512, 37.832564787218985],
-        [-122.49223709106445, 37.83337825839438],
-        [-122.49378204345702, 37.83368330777276]
-      ]
-    }
-  };
-
 
   let total_cost = 0
 
@@ -94,39 +45,7 @@ function TripSummary({trip}) {
           </div>
         </div>
 
-        <MapGL
-          style={{ width: '100%', height: '300px' }}
-          mapStyle='mapbox://styles/mapbox/light-v9'
-          accessToken="pk.eyJ1IjoiYWZ1cm8iLCJhIjoiY2t5ZW4zOWFpMGRqczJxcWtheWNvZHQ2aiJ9.hn3zYWxYhJZv0YcZAVQcsA"
-          latitude={viewport.latitude}
-          longitude={viewport.longitude}
-          zoom={viewport.zoom}
-          onViewportChange={setViewport}
-          {...viewport}
-        >
-          <Marker latitude={currentTrip.from_latitude} longitude={currentTrip.from_longitude}>
-            <div>📍</div>
-          </Marker>
-
-          <Marker latitude={currentTrip.to_latitude} longitude={currentTrip.to_longitude}>
-            <div>📍</div>
-          </Marker>
-
-          <Source id='route' type='geojson' data={data} />
-          <Layer
-            id='route'
-            type='line'
-            source='route'
-            layout={{
-              'line-join': 'round',
-              'line-cap': 'round'
-            }}
-            paint={{
-              'line-color': '#888',
-              'line-width': 8
-            }}
-          />
-        </MapGL>
+        <Map trip={trip} />
       </div>
     )
   }
